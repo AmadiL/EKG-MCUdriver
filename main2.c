e3d3dfef55940a18d820911d8a2d156867d5e34a
AgataGasecka <gaseckaagata@gmail.com>
2016-04-29 11:05:04 +0200

    siemka

--- main2.c
+++ main2.c
@@ -1,9 +1,6 @@
 
 #include <stdio.h>
-
 #include <windows.h>
-#include <conio.h>
-#include <math.h>
 #include <winbase.h>
 
 
@@ -11,6 +8,14 @@ static DCB dcb;
 //static HANDLE hNumPort;
 static char lpBuffor_read[33], lpBuffor_write[33];
 static DWORD RS_ile;
+
+void WritetoCOM (HANDLE *hNumPort, char* MSG) {
+	printf("2\n");
+	strcpy(lpBuffor_write, &MSG);// Niech to bêdzie przyk³adowe polecenie dla urz¹dzenia
+	printf("3\n");
+	WriteFile(hNumPort, lpBuffor_write, strlen(lpBuffor_write), &RS_ile, 0);
+	printf("4\n");	
+}
 
 HANDLE * CreateHandler(char* COM){
 	printf("1\n");
@@ -45,20 +50,29 @@ int main(){
 	printf("dupa\n");
 	HANDLE *hNumPort = CreateHandler(COM);
 	printf("dupa2\n");
+	char* MSG[30];
 	
 	SetCommState(hNumPort, &dcb);
+	
+	while(*MSG != "exit"){
+		scanf("%s", &MSG);
+		printf ("%s", MSG);
+		WritetoCOM(hNumPort, *MSG);
+		
+		//memset(*MSG, 0, 30);
+	}
+	/*
 	printf("2\n");
 	strcpy(lpBuffor_write, "CLS 30\r\nLINE 100 100 600 400 0\r\n");// Niech to bêdzie przyk³adowe polecenie dla urz¹dzenia
 	printf("3\n");
 	WriteFile(hNumPort, lpBuffor_write, strlen(lpBuffor_write), &RS_ile, 0);
 	printf("4\n");
-	
-	ReadFile(hNumPort, lpBuffor_read, 15, &RS_ile, 0);
+	*/
+	//ReadFile(hNumPort, lpBuffor_read, 15, &RS_ile, 0);
 	printf("5\n");
 	
 	CloseHandle(hNumPort);
 
-	//system("Pause");
 	getchar();
 	return 0;
 }
